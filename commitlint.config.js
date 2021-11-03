9df6a48a4d7f311882dc863a608500f7061e0108
Karan Balkar <karanbalkar@gmail.com>
2021-11-03 09:58:03 +0530

    Test Commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,8 @@
 module.exports = {
     parserPreset: "conventional-changelog-conventionalcommits",
     rules: {
+      "body-empty": [1, "never"],
       "body-leading-blank": [1, "always"],
-      "body-max-line-length": [2, "always", 100],
-      "footer-leading-blank": [1, "always"],
-      "footer-max-line-length": [2, "always", 100],
-      "header-max-length": [2, "always", 100],
       "subject-case": [
         2,
         "never",
